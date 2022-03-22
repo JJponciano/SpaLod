7ad6d335b811335e3610fb08cebc9f0a0fa886d7
Claire Ponciano <prudh.claire@gmail.com>
2022-03-22 12:37:30 +0100

    Create MatchingDataModel.java

---
+++ src/main/java/info/ponciano/lab/Spalodwfs/MatchingDataModel.java
@@ -0,0 +1,27 @@
+package info.ponciano.lab.Spalodwfs;
+
+public class MatchingDataModel {
+    public String input;
+    public String value;
+
+    public MatchingDataModel(String input, String value) {
+        this.input = input;
+        this.value = value;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
