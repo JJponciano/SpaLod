de5e8eb7b55f65058d5c167e455feb7d21289d7e
Claire Ponciano <prudh.claire@gmail.com>
2022-04-06 15:19:23 +0200

    Update EnrichController.java

--- src/main/java/info/ponciano/lab/Spalodwfs/enrichment/EnrichController.java
+++ src/main/java/info/ponciano/lab/Spalodwfs/enrichment/EnrichController.java
@@ -1,13 +1,15 @@
-package info.ponciano.lab.Spalodwfs;
+package info.ponciano.lab.Spalodwfs.enrichment;
 
 import info.ponciano.lab.Spalodwfs.geotime.controllers.last.GeoJsonController;
 import info.ponciano.lab.Spalodwfs.geotime.controllers.storage.StorageService;
 import info.ponciano.lab.Spalodwfs.geotime.models.semantic.KB;
 import info.ponciano.lab.Spalodwfs.geotime.models.semantic.OntoManagementException;
+import info.ponciano.lab.pisemantic.PiOntologyException;
 import info.ponciano.lab.pisemantic.PiSparql;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +23,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -52,14 +55,15 @@ public class EnrichController {
             String file_path = KB.STORAGE_DIR + "/" + filename;
             //Read the ontology
             PiSparql ont = new PiSparql(file_path);
+            OntModel kb = KB.get().getOnt().getOnt();
 
             //init list of data_unknown properties and classes
-           MatchingDataCreationDto data_unknown = new MatchingDataCreationDto();
+            MatchingDataCreationDto data_unknown = new MatchingDataCreationDto();
             List<String[]> remainingData = new ArrayList<>();
 
             //extract subject predicate and object for each individuals
             ResultSet select = ont.select("Select ?s ?p ?o WHERE{?s ?p ?o}");
-            OntModel kb = KB.get().getOnt().getOnt();
+
 
             while (select.hasNext()) {
                 QuerySolution next = select.next();
@@ -67,33 +71,29 @@ public class EnrichController {
                 Resource p = next.getResource("?p");
                 RDFNode o = next.get("?o");
                 //test if the property is known in the ontology
-                boolean unknownP = kb.getOntProperty(p.getURI()) == null;
+                if (p.getURI().toLowerCase().contains("test"))
+                    System.out.println("here");
+                boolean unknownP =kb.containsResource(p);
                 if (unknownP) {
-                    data_unknown.add(new MatchingDataModel(p.getURI(),""));
+                    data_unknown.add(new MatchingDataModel(p.getURI(), ""));
                 }
-                 if( o.isResource()&&o.asResource().getURI()==null){
-                     System.err.printf("------------- URI NULL FOR: "+o);
-                 }else {
-                     boolean unknowC = o.isResource() && kb.getOntClass(o.asResource().getURI()) == null;
-                     if (unknowC) {
-                         data_unknown.add(new MatchingDataModel(o.asResource().getURI(),""));
-                     }
-                     //if the property and the class(if it is ) are known, add it to the ontology.
-                     if (!unknownP && (!o.isResource() || unknowC)) {
-//                    if (o.isResource()) {
-//                        String query1 = "INSERT DATA{<" + s.getURI() + "> <" + p.getURI() + "><" + o.asResource().getURI() + ">}";
-//                        ont.update(query1);
-//                    } else {
-//                        String query = "INSERT DATA{<" + s.getURI() + "> <" + p.getURI() + ">" + o.asLiteral() + "}";
-//                        ont.update(query);
-//                    }
-                     } else {
-                         if (o.isResource())
-                             remainingData.add(new String[]{s.getURI(), p.getURI(), o.asResource().getURI()});
-                         else
-                             remainingData.add(new String[]{s.getURI(), p.getURI(), o.asLiteral().toString()});
-                     }
-                 }
+                if (o.isResource() && o.asResource().getURI() == null) {
+                    System.err.printf("------------- URI NULL FOR: " + o);
+                } else {
+                    boolean unknownClass = o.isResource() && kb.getResource(o.asResource().getURI()) == null;
+                    if (unknownClass) {
+                        data_unknown.add(new MatchingDataModel(o.asResource().getURI(), ""));
+                    }
+                    //if the property and the class(if it is ) are known, add it to the ontology.
+                    if (!unknownP && (!o.isResource() || unknownClass)) {
+//                         update(ont, s, p, o);
+                    } else {
+                        if (o.isResource())
+                            remainingData.add(new String[]{s.getURI(), p.getURI(), o.asResource().getURI()});
+                        else
+                            remainingData.add(new String[]{s.getURI(), p.getURI(), o.asLiteral().toString()});
+                    }
+                }
             }
             KB.get().save();
 
@@ -109,6 +109,17 @@ public class EnrichController {
             return "error";
         }
     }
+
+    private void update(PiSparql ont, Resource s, Resource p, RDFNode o) throws PiOntologyException {
+        if (o.isResource()) {
+            String query1 = "INSERT DATA{<" + s.getURI() + "> <" + p.getURI() + "><" + o.asResource().getURI() + ">}";
+            ont.update(query1);
+        } else {
+            String query = "INSERT DATA{<" + s.getURI() + "> <" + p.getURI() + ">" + o.asLiteral() + "}";
+            ont.update(query);
+        }
+    }
+
     // initialize the model attribute "dataindiv"
     @ModelAttribute(name = "vocMatching")
     public MatchingForm vocMatching() {
