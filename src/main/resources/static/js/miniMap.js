344b604063a0fdb94cfee7b8b270595013b601d2
Dincky <83808613+Dincky@users.noreply.github.com>
2022-05-18 15:14:06 +0200

    Add TopPlus Layer
    Add layer TopPlus

--- src/main/resources/static/js/miniMap.js
+++ src/main/resources/static/js/miniMap.js
@@ -5,7 +5,9 @@ map.on('baselayerchange', function (e) {
         miniMap.changeLayer(mini_grayscale);
     } else if (e.name == "Relief"){
         miniMap.changeLayer(mini_relief);
-    } else {
+    } else if (e.name == "Streets"){
         miniMap.changeLayer(mini_streets);
+    } else if (e.name == "Topplus"){
+        miniMap.changeLayer(mini_topplus);
     }
 });
