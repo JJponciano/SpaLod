acac8b9d28e4cf4a2f06f89ebf88645b14123ecf
Claire Ponciano <prudh.claire@gmail.com>
2022-09-18 22:35:46 +0200

    Create Progress.java

---
+++ src/main/java/info/ponciano/lab/Spalodwfs/enrichment/Progress.java
@@ -0,0 +1,35 @@
+package info.ponciano.lab.Spalodwfs.enrichment;
+
+
+public class Progress {
+    private int value;
+    private String message;
+
+    public Progress(int value, String message) {
+        this.value = value;
+        this.message = message;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    @Override
+    public String toString() {
+        return "Progress{" +
+                "value=" + value +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
