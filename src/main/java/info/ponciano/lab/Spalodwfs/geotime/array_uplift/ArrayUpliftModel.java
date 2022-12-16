29053784f53b6c68f9b78908c27de09e3e7b14b0
Claire Ponciano <ponciano.jeanjacques@gmail.com>
2022-12-16 09:10:28 +0100

    Update ArrayUpliftModel.java

--- src/main/java/info/ponciano/lab/Spalodwfs/geotime/array_uplift/ArrayUpliftModel.java
+++ src/main/java/info/ponciano/lab/Spalodwfs/geotime/array_uplift/ArrayUpliftModel.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301  USA
  */
-package info.ponciano.lab.Spalodwfs.geotime.array_uplift;
+package info.ponciano.lab.spalodwfs.geotime.array_uplift;
 
 import java.io.FileNotFoundException;
 import java.util.HashMap;
