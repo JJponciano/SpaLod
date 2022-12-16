ed9178b5e3d6cfe0b112f40616a2992cb91f1b6d
Claire Ponciano <ponciano.jeanjacques@gmail.com>
2022-12-16 09:10:44 +0100

    Update StorageException.java

--- src/main/java/info/ponciano/lab/Spalodwfs/geotime/controllers/storage/StorageException.java
+++ src/main/java/info/ponciano/lab/Spalodwfs/geotime/controllers/storage/StorageException.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301  USA
  */
-package info.ponciano.lab.Spalodwfs.geotime.controllers.storage;
+package info.ponciano.lab.spalodwfs.geotime.controllers.storage;
 
 public class StorageException extends RuntimeException {
 
