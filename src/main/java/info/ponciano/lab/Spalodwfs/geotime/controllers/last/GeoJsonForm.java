23587c73109ec7303c9f64a7312bb2b806e5d548
Claire Ponciano <ponciano.jeanjacques@gmail.com>
2022-12-16 09:10:35 +0100

    Update GeoJsonForm.java

--- src/main/java/info/ponciano/lab/Spalodwfs/geotime/controllers/last/GeoJsonForm.java
+++ src/main/java/info/ponciano/lab/Spalodwfs/geotime/controllers/last/GeoJsonForm.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301  USA
  */
-package info.ponciano.lab.Spalodwfs.geotime.controllers.last;
+package info.ponciano.lab.spalodwfs.geotime.controllers.last;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
