4b60475aa3a26acb05df75af70808fce3a4e9bd0
Federdispi <federdispi@gmail.com>
2023-04-13 17:23:19 +0200

    User Actions View Created
    This component allows the user to do some actions, like filter or add data

--- src/main/vue_js/spalod/src/App.vue
+++ src/main/vue_js/spalod/src/App.vue
@@ -1,44 +1,39 @@
 <script setup>
 import NavBar from './components/NavBar.vue';
+import UserActions from './components/UserActions.vue';
 
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <NavBar></NavBar>
+  <div class="app">
+    <NavBar></NavBar>
+    <div class="main">
+      <div class="user-actions-container">
+        <UserActions></UserActions>
+      </div>
     </div>
-  </header>
-
-  <main>
-  </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.main {
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+}
+
+.user-actions-container {
+  flex: 1;
+  padding: 20px;
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+  
 }
 </style>
---
+++ src/main/vue_js/spalod/src/components/UserActions.vue
@@ -0,0 +1,104 @@
+<template>
+    <div class="user-actions" :class="{dark: isDarkMode}">
+        <div class="search-bar">
+            <input type="text" v-model="searchText" placeholder="Search..." />
+            <button class="search-button" @click="search">Search</button>
+        </div>
+        <button @click="filterData">Filter</button>
+        <button @click="addData">Add Data</button>
+        <button @click="confirmRequest">Confirm Request</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            searchText: '',
+            isDarkMode: false,
+        };
+    },
+    mounted() {
+        window.matchMedia('(prefers-color-scheme: dark)').addListener(event => {
+            this.isDarkMode = event.matches;
+        });
+    },
+    methods: {
+        search() {
+            // TODO: Implement search
+        },
+        filterData() {
+            // TODO: Implement filter
+        },
+        addData() {
+            // TODO: Implement add data
+        },
+        confirmRequest() {
+            // TODO: Implement confirm request
+        },
+    },
+};
+</script>
+
+<style scoped>
+.user-actions {
+    padding: 20px;
+    border-radius: 5px;
+    flex-direction: column;
+    display: flex;
+    align-items: start;
+}
+
+.user-actions.dark {
+    background-color: #1A202C;
+    color: #fff;
+}
+
+.user-actions.light {
+    background-color: #fff;
+    color: #1A202C;
+}
+
+.search-bar {
+    display: flex;
+    margin-bottom: 20px;
+}
+
+input[type="text"] {
+    flex: 1;
+    padding: 10px;
+    border: none;
+    border-radius: 5px 0 0 5px;
+    background-color: #E5E7EB;
+    color: #1F2937;
+}
+
+.search-button {
+    padding: 10px 20px;
+    border-radius: 0 5px 5px 0;
+    border: none;
+    background-color: #4A5568;
+    color: #fff;
+    cursor: pointer;
+    transition: background-color 0.2s ease-in-out;
+    font-size: 18px;
+    font-weight: bold;
+}
+
+button {
+    padding: 10px 20px;
+    border-radius: 5px;
+    border: none;
+    background-color: transparent;
+    color: inherit;
+    cursor: pointer;
+    transition: background-color 0.2s ease-in-out;
+    font-size: 18px;
+    font-weight: bold;
+}
+
+button:hover {
+    background-color: #4A5568;
+    color: #fff;
+}
+</style>
