50ef97d1c9dc65df77f808716b616ed05172a007
Dr. Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2023-04-17 18:46:47 +0200

    Update WebConfig.java

--- src/main/java/info/ponciano/lab/spalodwfs/WebConfig.java
+++ src/main/java/info/ponciano/lab/spalodwfs/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8080") // Replace with the URL of the frontend application
+                        .allowedOrigins("*") // Replace with the URL of the frontend application
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
