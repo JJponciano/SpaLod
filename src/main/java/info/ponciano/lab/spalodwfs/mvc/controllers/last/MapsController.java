c648a73c87228cfb29651517d2961bee86664abd
Dr. Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2023-04-17 18:45:39 +0200

    Update MapsController.java

--- src/main/java/info/ponciano/lab/spalodwfs/mvc/controllers/last/MapsController.java
+++ src/main/java/info/ponciano/lab/spalodwfs/mvc/controllers/last/MapsController.java
@@ -18,7 +18,7 @@
  */
 package info.ponciano.lab.spalodwfs.mvc.controllers.last;
 
-import info.ponciano.lab.spalodwfs.mvc.controllers.storage.StorageService;
+import info.ponciano.lab.spalodwfs.controller.storage.StorageService;
 import info.ponciano.lab.spalodwfs.mvc.models.geojson.GeoJsonRDF;
 import info.ponciano.lab.spalodwfs.mvc.models.semantic.KB;
 import info.ponciano.lab.pisemantic.PiOnt;
