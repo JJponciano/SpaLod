478261a4f161cd004b6b03ad40f76b194bf60cc9
Federdispi <federdispi@gmail.com>
2023-04-17 17:39:55 +0200

    Show a map
    The map component now shows a map with all the different layers

--- src/main/vue_js/spalod/src/components/MapView.vue
+++ src/main/vue_js/spalod/src/components/MapView.vue
@@ -1,7 +1,72 @@
+<script>
+import L from 'leaflet';
+
+export default {
+    data() {
+        return {
+            dataArray: 'default',
+            sizeOfArray: 0,
+        };
+    },
+    mounted() {
+        // Creation of one layer of points
+        var query = new L.layerGroup();
+
+        // Custom icon class
+        var MarkerIcon = L.Icon.extend({
+            options: {
+                iconSize: [20, 20],
+                shadowSize: [0, 0],
+                iconAnchor: [10, 10],
+                popupAnchor: [0, -10],
+            }
+        });
+
+        var mbAttr = 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+                                    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            mbUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZmVkZXJkaXNwaSIsImEiOiJjbGdreWlncHcwd3F0M2hsdnhscGg5Yzc1In0.Ud5vRdMf9cbtUUd5ufgKXQ';
+
+        var rUrl = 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png';
+
+        var topplusUrl = 'http://sgx.geodatenzentrum.de/wms_topplus_web_open';
+        var topplusAttr = '&copy Bundesamt für Kartographie und Geodäsie 2017, <a href="http://sg.geodatenzentrum.de/web_public/Datenquellen_TopPlus_Open.pdf">Datenquellen</a>';
+
+        var grayscale = L.tileLayer(mbUrl, {id: 'mapbox/light-v9', tileSize: 512, zoomOffset: -1, attribution: mbAttr}),
+            streets = L.tileLayer(mbUrl, {id: 'mapbox/streets-v11', tileSize: 512, zoomOffset: -1, attribution: mbAttr}),
+            relief = L.tileLayer(rUrl, {maxZoom: 17, attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'}),
+            topplus = L.tileLayer.wms(topplusUrl, {layers: 'web', format: 'image/png',transparent: true, attribution: topplusAttr});
+                      
+        // Definition of the map, and setup of details : coordinates of the center, zoom level and layers that appear on loading
+        var map = L.map('map', {
+            center: [49.99, 8.24],
+            zoom: 6,
+            minZoom: 2,
+            fullscreenControl: true,
+            layers: [streets, query]
+        });
+
+        // Definition of layers
+        var baseLayers = {
+            "Grayscale": grayscale,
+            "Streets": streets,
+            "Relief" : relief,
+            "Topplus" : topplus
+        };
+
+        // Definition of point layers
+        var overlays = {
+            "Query": query
+        };
+
+        // Add layers and points layers to the map
+        L.control.layers(baseLayers, overlays).addTo(map);
+    },
+};
+</script>
+
 <template>
-    <div class="map-view">
+    <div class="map-view" id="map">
         <h2>Map</h2>
-        <!-- TODO -->
     </div>
 </template>
 
--- src/main/vue_js/spalod/src/main.js
+++ src/main/vue_js/spalod/src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import './assets/main.css'
+import 'leaflet/dist/leaflet.css';
 
 createApp(App).mount('#app')
