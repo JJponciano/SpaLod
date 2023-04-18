fad53de5bbea41acebeff9de1725a587b9357171
youneskamli <youneskamli16@gmail.com>
2023-04-18 10:20:43 +0200

    légère modif sur l'auth

--- src/main/java/info/ponciano/lab/Spalodwfs/geotime/controllers/security/WebSecurityConfig.java
+++ src/main/java/info/ponciano/lab/Spalodwfs/geotime/controllers/security/WebSecurityConfig.java
@@ -147,7 +147,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
-			.withUser("*").password(passwordEncoder().encode("*"))
+			.withUser("user").password(passwordEncoder().encode("user123"))
 			.roles("USER")
 			.and()
 			.withUser("admin").password(passwordEncoder().encode("admin123"))
