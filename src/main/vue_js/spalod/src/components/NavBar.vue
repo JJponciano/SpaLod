39b80e02c00351456bd4f050a64b8d3df0ea63bc
Pierrot12000 <64778478+Pierrot12000@users.noreply.github.com>
2023-04-24 23:40:58 +0200

    fix broken menu (NavBar.vue)

--- src/main/vue_js/spalod/src/components/NavBar.vue
+++ src/main/vue_js/spalod/src/components/NavBar.vue
@@ -5,7 +5,7 @@
         <span></span>
         <span></span>
       </button>
-      <ul :class="['menuopen',menuAnimationClass]">
+      <ul :class="['menuopen',menuAnimationClass]" @transitionend="onTransitionend">
         <li><button @click="navigateTo('public')" :class="{ active: activeTab === 'public' }">Public</button></li>
         <li><button @click="navigateTo('doc')" :class="{ active: activeTab === 'doc' }">Doc</button></li>
         <li><button @click="navigateTo('external')" :class="{ active: activeTab === 'external' }">External Links</button></li>
@@ -20,7 +20,7 @@
       return {
         activeTab: 'admin',
         isDarkMode: false,
-        menuOpen: false,
+        menuOpen: "menu-closed",
         isAdmin: true,
         menuAnimationClass: ""
       };
@@ -36,8 +36,8 @@
         this.isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
       },
       togglemenu(){
-        this.menuOpen=!this.menuOpen;
-        this.menuAnimationClass = this.menuOpen ? 'slide-in-left' : 'slide-out-left';
+        this.menuOpen=this.menuOpen === "menu-closed" ? "menu-open" : "menu-closed";;
+        this.menuAnimationClass = this.menuOpen === "menu-open" ? "slide-in-left" : "slide-out-left";
       },
       closeMenu(){
         this.menuOpen=false;
@@ -52,6 +52,7 @@
   <style scoped>
   .navbar {
     display: flex;
+    background-color:#fff;
     justify-content: space-between;
     align-items: center;
     padding: 10px;
@@ -67,7 +68,7 @@
   }
   
   .navbar.light {
-    background-color: #fff;
+    background-color:#fff;
     color: #1A202C;
   }
   
@@ -105,7 +106,6 @@
   display: inline-block;
   cursor: pointer;
   padding: 30px 42px 10px 10px;
-  background-color: transparent;
   border: 1px solid #000;
   border-radius: 5px;
   transition: background-color 0.2s ease-in-out;
@@ -149,48 +149,65 @@
   transform: translateY(-50%);
 }
 .slide-in-left {
-  animation: slide-in-left 0.8s forwards;
+  animation: slide-in-left 0.7s forwards;
 }
 
 .slide-out-left {
-  animation: slide-out-left 0.8s forwards;
+  animation: slide-out-left 0.7s forwards;
 }
 
 @keyframes slide-in-left {
   from {
-    transform: translateX(-100%);
+    transform: translateX(0%);
   }
   to {
-    transform: translateX(0);
+    transform: translateX(+100%);
   }
 }
 
 @keyframes slide-out-left {
   from {
-    transform: translateX(0);
+    transform: translateX(100%);
   }
   to {
-    transform: translateX(-100%);
+    transform: translateX(0%);
   }
 }
 
 ul {
   position: absolute;
+  bottom:-90vh;
   top: 100%;
-  left: 0%;
-  display: none;
+  left: -220px;
   padding: 10px;
   background-color: #fff;
   border: 1px solid #000;
   border-radius: 5px;
   box-shadow: 0px 2px 5px rgba(0,0,0,0.3);
 }
-.menuopen{
-  display: block;
+ul button{
+  padding: 10px 20px;
+  border-radius: 5px;
+  border: none;
+  background-color: transparent;
+  color: inherit;
+  cursor: pointer;
+  transition: background-color 0.2s ease-in-out;
+  font-size: 18px;
+  font-weight: bold;
+  margin-top: 10px;
+  width: 100%;
+  text-align: left;
+}
+.navbar.dark ul{
+  background-color:#4A5568;
+}
+.navbar.light ul{
+  background-color: aliceblue;
 }
 
 ul li {
-  margin: 20px 20px 0px 20px;
+  margin: 20px 10px 20px 20px;
 }
   </style>
   
