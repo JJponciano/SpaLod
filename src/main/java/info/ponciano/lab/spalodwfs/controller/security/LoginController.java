5275728de25b941f9e46db9c52fe068bf9994435
youneskamli <youneskamli16@gmail.com>
2023-05-25 12:46:40 +0200

    update LoginController

--- src/main/java/info/ponciano/lab/spalodwfs/controller/security/LoginController.java
+++ src/main/java/info/ponciano/lab/spalodwfs/controller/security/LoginController.java
@@ -20,12 +20,10 @@ package info.ponciano.lab.spalodwfs.controller.security;
 
 import java.io.IOException;
 import java.security.Principal;
-import java.util.Collections;
 import java.util.Map;
 
 import javax.annotation.security.RolesAllowed;
 
-import org.apache.jena.atlas.json.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -33,20 +31,15 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 @RestController
 public class LoginController
