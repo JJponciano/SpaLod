27c8c42d855cdf9cb2e0b0c51032d9a4f5a17268
Dr. Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2023-07-19 16:28:39 +0200

    Create JsonToGeoJsonTest.java

---
+++ src/test/java/info/ponciano/lab/spalodwfs/model/JsonToGeoJsonTest.java
@@ -0,0 +1,34 @@
+package info.ponciano.lab.spalodwfs.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.FileReader;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.Test;
+
+import com.google.gson.JsonObject;
+
+import org.junit.jupiter.api.Test;
+
+public class JsonToGeoJsonTest {
+    @Test
+    void testConvertJsonToGeoJson() throws Exception {
+        String inputPath = "src/test/resources/test.json";
+        String outputPath = "src/test/resources/test_output.json";
+        String expectedOutputPath = "src/test/resources/test_geo.json";
+
+        // Run the method to test
+        JsonToGeoJson.convertJsonToGeoJson(inputPath, outputPath);
+
+        // Load the output and expected output
+        JSONParser parser = new JSONParser();
+        JSONObject actualOutput = (JSONObject) parser.parse(new FileReader(outputPath));
+        JSONObject expectedOutput = (JSONObject) parser.parse(new FileReader(expectedOutputPath));
+
+        // Compare the output with the expected output
+        assertEquals(expectedOutput, actualOutput);
+    }
+}
