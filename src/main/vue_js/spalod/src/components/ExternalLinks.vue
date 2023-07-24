abd083efe54c38140a5507a250d825e7baf47a94
Dr. Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2023-07-24 12:43:55 +0200

    Update ExternalLinks.vue

--- src/main/vue_js/spalod/src/components/ExternalLinks.vue
+++ src/main/vue_js/spalod/src/components/ExternalLinks.vue
@@ -1,17 +1,27 @@
 <template>
     <div class="link">
-        <ul>
-            <li>
-                <p>
-                    i3mainz: <a href="https://i3mainz.hs-mainz.de/" target="_blank">https://i3mainz.hs-mainz.de/</a>
-                </p>
-            </li>
-            <li>
-                <p>
-                    Leaflet: <a href="https://leafletjs.com/" target="_blank">https://leafletjs.com/</a>
-                </p>
-            </li>
-        </ul>
+        <ul class="link-list">
+      <li>
+        <span class="link-title">i3mainz:</span>
+        <a href="https://i3mainz.hs-mainz.de/" target="_blank" class="link-url">https://i3mainz.hs-mainz.de/</a>
+      </li>
+      <li>
+        <span class="link-title">Leaflet:</span>
+        <a href="https://leafletjs.com/" target="_blank" class="link-url">https://leafletjs.com/</a>
+      </li>
+      <li>
+        <span class="link-title">Wikidata endpoint:</span>
+        <a href="https://query.wikidata.org/" target="_blank" class="link-url">https://query.wikidata.org/</a>
+      </li>
+      <li>
+        <span class="link-title">DBpedia endpoint:</span>
+        <a href="https://dbpedia.org/sparql" target="_blank" class="link-url">https://dbpedia.org/sparql</a>
+      </li>
+      <li>
+        <span class="link-title">GDI-DE, Geodateninfrastruktur Deutschland:</span>
+        <a href="https://www.gdi-de.org/" target="_blank" class="link-url">https://www.gdi-de.org/</a>
+      </li>
+    </ul>
       </div>
 </template>
 
