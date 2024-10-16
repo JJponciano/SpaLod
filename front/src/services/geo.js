11658bbab3a977c144377858c16701ae5163a3f2
vvv <valentin.blind@gmail.com>
2024-10-16 18:46:30 +0200

    more generic sparql handler

--- front/src/services/geo.js
+++ front/src/services/geo.js
@@ -4,48 +4,55 @@ export async function getAllGeo() {
   const req = await $fetch("api/geo/all");
   const result = await req.json();
 
-  return result.results.bindings.map(({ feature, wkt }) => {
-    const res = {
-      id: feature.value,
-    };
-
-    // TODO: handle other types
-    if (wkt.value.startsWith("LINESTRING")) {
-      res.type = "LINESTRING";
-
-      res.geo = wkt.value.split(",").map((x) =>
-        x
-          .trim()
-          .replace("LINESTRING (", "")
-          .replace(")", "")
-          .split(" ")
-          .map((y) => Number(y))
-      );
-    } else if (wkt.value.startsWith("MULTILINESTRING")) {
-      res.type = "MULTILINESTRING";
-
-      res.geo = wkt.value.split(/\).*?,.*?\(/).map((x) =>
-        x.split(",").map((y) =>
-          y
-            .trim()
-            .replace("MULTILINESTRING ((", "")
-            .replace("))", "")
-            .split(" ")
-            .map((z) => Number(z))
-        )
-      );
-    }
-
-    return res;
-  });
+  return getGeoData(result);
 }
 
 export async function getFeature(id) {
   const req = await $fetch(`api/geo/feature?id=${id}`);
   const result = await req.json();
 
-  return result.results.bindings.map(({ key, value }) => ({
-    key: key.value,
-    value: value.value,
-  }));
+  return getGeoData(result);
+}
+
+export function getGeoData(sparqlResult) {
+  const headers = sparqlResult.head.vars;
+
+  const res = [];
+
+  for (const binding of sparqlResult.results.bindings) {
+    const itemRes = {
+      metadatas: {},
+    };
+    for (const header of headers) {
+      if (binding[header].value.startsWith("LINESTRING")) {
+        itemRes.type = "LINESTRING";
+        itemRes.geo = binding[header].value.split(",").map((x) =>
+          x
+            .trim()
+            .replace("LINESTRING (", "")
+            .replace(")", "")
+            .split(" ")
+            .map((y) => Number(y))
+        );
+      } else if (binding[header].value.startsWith("MULTILINESTRING")) {
+        itemRes.type = "MULTILINESTRING";
+        itemRes.geo = binding[header].value.split(/\).*?,.*?\(/).map((x) =>
+          x.split(",").map((y) =>
+            y
+              .trim()
+              .replace("MULTILINESTRING ((", "")
+              .replace("))", "")
+              .split(" ")
+              .map((z) => Number(z))
+          )
+        );
+      } else {
+        itemRes.metadatas[header] = binding[header].value;
+      }
+    }
+
+    res.push(itemRes);
+  }
+
+  return res;
 }
